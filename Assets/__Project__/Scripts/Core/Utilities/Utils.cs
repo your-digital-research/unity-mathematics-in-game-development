0ae90e9f291781a303a7947895753e274428deb4
Davit Badalyan <davit.badalyan.00@gmail.com>
2023-08-31 16:44:09 +0400

    Summary for function

--- Assets/__Project__/Scripts/Core/Utilities/Utils.cs
+++ Assets/__Project__/Scripts/Core/Utilities/Utils.cs
@@ -4,12 +4,23 @@ namespace Core.Utilities
 {
     public static class Utils
     {
-        public static float DotProduct(Vector3 p0, Vector3 p1, Vector3 c)
+        /// <summary>
+        /// The Dot Product (Scalar Product) between vector A and vector B
+        /// is equal to the sum of the products of each component.
+        /// </summary>
+        /// <param name="firstPoint">First Point of Product</param>
+        /// <param name="secondPoint">Second Point of Product</param>
+        /// <param name="centerPoint">Reference (Center) Point of Product</param>
+        /// <returns>
+        /// Returns the value of the cosine of the angle between two-unit vectors.
+        /// So its result lies within a range from -1f to 1f
+        /// </returns>
+        public static float DotProduct(Vector3 firstPoint, Vector3 secondPoint, Vector3 centerPoint)
         {
-            Vector3 a = (p0 - c).normalized;
-            Vector3 b = (p1 - c).normalized;
+            Vector3 firstVector = (firstPoint - centerPoint).normalized;
+            Vector3 secondVector = (secondPoint - centerPoint).normalized;
 
-            return (a.x * b.x) + (a.y * b.y) + (a.z * b.z);
+            return (firstVector.x * secondVector.x) + (firstVector.y * secondVector.y) + (firstVector.z * secondVector.z);
         }
     }
 }
