0b91b6e7eef56cb90e896cbe4fd3d3f4c14466bb
Davit Badalyan <davit.badalyan.00@gmail.com>
2024-01-05 17:09:11 +0400

    Cross Product Usage example initial setup

--- Assets/__Project__/Scripts/Core/Types/ExampleScene.cs
+++ Assets/__Project__/Scripts/Core/Types/ExampleScene.cs
@@ -6,5 +6,6 @@ namespace Core.Types
         DotProductVisualisationScene,
         CrossProductVisualisationScene,
         DotProductUsageScene,
+        CrossProductUsageScene,
     }
 }
---
+++ Assets/__Project__/Scripts/Core/UI/Views/CrossProductUsageView.cs
@@ -0,0 +1,73 @@
+using Core.Constants;
+using System;
+using UnityEngine;
+using Zenject;
+using UniRx;
+
+namespace Core.UI
+{
+    public class CrossProductUsageView : MonoBehaviour
+    {
+        #region SERIALIZED_VARIABLES
+
+        [Header("References")]
+        [SerializeField] private ControlPanelTab controlPanelTab;
+
+        #endregion
+
+        #region PRIVATE_VARIABLES
+
+        private TransitionView _transitionView;
+
+        #endregion
+
+        #region ZENJECT
+
+        [Inject]
+        private void Constructor(TransitionView transitionView)
+        {
+            _transitionView = transitionView;
+        }
+
+        #endregion
+
+        #region MONO
+
+        private void Start()
+        {
+            Observable.Timer(TimeSpan.FromSeconds(Constant.InitDelay)).Subscribe(_ =>
+            {
+                _transitionView.Hide();
+
+                Init();
+            });
+        }
+
+        #endregion
+
+        #region PUBLIC_FUNCTIONS
+
+        public void OnControlPanelButtonClick()
+        {
+            ToggleControlPanel();
+        }
+
+        #endregion
+
+        #region PRIVATE_FUNCTIONS
+
+        private void Init()
+        {
+            //
+        }
+
+        private void ToggleControlPanel()
+        {
+            if (!controlPanelTab.IsStable) return;
+
+            controlPanelTab.Toggle(!controlPanelTab.IsVisible);
+        }
+
+        #endregion
+    }
+}
